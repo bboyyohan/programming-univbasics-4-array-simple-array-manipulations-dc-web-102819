ad1a23fd6a86f801832e850072934793ddb33b67
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-24 00:05:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array, array2)
 end
 
 def using_insert(array, mouse)
-  array.insert(4, mouse)
+  array.insert(4)
 end
 
 def using_uniq(array)
@@ -37,4 +37,6 @@ end
 def using_flatten(array)
   array.flatten()
 end
+
+
 
